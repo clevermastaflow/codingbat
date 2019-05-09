15bef624ed583ddf1b374d085bac7645a1dc5eaa
clevermastaflow <50432725+clevermastaflow@users.noreply.github.com>
2019-05-08 22:36:03 -0400

    Update powerN.java

--- java/recursion-1/powerN.java
+++ java/recursion-1/powerN.java
@@ -1,9 +1,22 @@
 /* Given base and n that are both 1 or more, compute recursively (no loops) 
  * the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
  */
-public int powerN(int base, int n) {
+/* public int powerN(int base, int n) {
     if(n <= 1)
         return base;
           
     return base * powerN(base, n - 1);
+}
+*/
+
+
+//=====
+//Try to use this
+public int powerN(int base, int n) {
+  if(base ==0 && n!=0)
+    return 0;
+  if(base !=0 && n==0) 
+    return 1;
+  return base * powerN(base, n -1);
+  
 }
